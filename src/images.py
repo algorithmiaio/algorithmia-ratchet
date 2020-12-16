6046b9aa93d3df046216462b4978b7c6bff73d9c
James Sutton <james@algorithmia.com>
2020-12-16 12:43:56 -0600

    added prod images to images.py file

--- src/images.py
+++ src/images.py
@@ -1,15 +1,15 @@
 
 PROD_IMAGES={
-    "python3": "",
-    "python2": "",
-    "pytorch-1.7": "",
-    "tensorflow-1.14": "",
-    "tensorflow-2.3": ""
+    "python3": "7498490e-90fe-4b8e-b65d-3ebf3ea6c5cb",
+    "python2": "e9088b81-b781-4df3-8cda-c064f607244b",
+    "pytorch-1.7": "f825234b-e2c4-4a61-83a6-74378514dcf0",
+    "tensorflow-1.14": "7885d20f-5165-4bcd-b5ac-0c5fb847b883",
+    "tensorflow-2.3": "2c907a75-902c-4d8c-8f13-512f73cfa989"
 }
 TEST_IMAGES={
     "python3": "4beb6189-3e18-4a7a-a466-473bebe68a9f",
     "python2": "f7ed7c9b-d205-48ef-9ddf-61880b96c1f4",
-    "pytorch": "5b027084-5fa7-431a-bdbd-f4f8eefc6ae1",
+    "pytorch-1.7": "5b027084-5fa7-431a-bdbd-f4f8eefc6ae1",
     "tensorflow-1.14": "27d3d02d-1da0-4868-986e-e0c6937e7d16",
     "tensorflow-2.3": "e9c1291c-1295-41b9-a4c5-30f7f44920aa"
 }
