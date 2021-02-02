a66cb26bbcfa2672c48146dcb3463c8c2becbb79
zeryx <james@algorithmia.com>
2021-02-02 14:03:12 -0400

    updated changes

--- algorithmia_benchmark.py
+++ algorithmia_benchmark.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import json
 import tarfile
 import shutil
+import sys
 from os import environ, path
 from src.algorithm_creation import initialize_algorithm, migrate_datafiles, update_algorithm
 from src.algorithm_testing import algorithm_test, algorithm_publish, call_algo
@@ -10,12 +11,11 @@ from src.algorithm_testing import algorithm_test, algorithm_publish, call_algo
 WORKING_DIR = "/tmp/QA_TEMPLATE_WORKDIR"
 
 
-
-
 def get_workflow(workflow_name):
     with open(f"workflows/{workflow_name}.json") as f:
         workflow_data = json.load(f)
     return workflow_data
+
 
 def find_algo(algo_name, artifact_path):
     local_path = f"algorithms/{algo_name}"
@@ -25,10 +25,11 @@ def find_algo(algo_name, artifact_path):
     else:
         raise Exception(f"algorithm {algo_name} not found in local cache (algorithms)")
 
+
 def create_workflow(workflow, source_client, destination_aems_master, destination_client):
     entrypoint_path = workflow['test_info'].get("entrypoint", None)
     entrypoint = None
-    for algorithm in tqdm(workflow.get("algorithms", [])):
+    for algorithm in workflow.get("algorithms", []):
         print("\n")
         algorithm_name = algorithm['name']
         remote_code_path = algorithm.get("code", None)
@@ -47,7 +48,7 @@ def create_workflow(workflow, source_client, destination_aems_master, destinatio
             find_algo(algorithm_name, artifact_path)
 
         print("initializing algorithm...")
-        algo_object = initialize_algorithm(algorithm_name, language, destination_aems_master,  destination_client)
+        algo_object = initialize_algorithm(algorithm_name, language, destination_aems_master, destination_client)
         print("migrating datafiles...")
         migrate_datafiles(algo_object, data_file_paths, source_client, destination_client, WORKING_DIR)
         print("updating algorithm source...")
@@ -86,10 +87,15 @@ if __name__ == "__main__":
     destination_api_key = environ.get("DESTINATION_API_KEY")
     destination_ca_cert = environ.get("DESTINATION_CA_CERT", None)
     destination_aems_master = environ.get("DESTINATION_AEMS_MASTER", "prod")
-    workflow_name = environ.get("WORKFLOW_NAME", "image_parallel_pipelining")
+    if len(sys.argv) > 1:
+        workflow_name = str(sys.argv[1])
+    else:
+        raise Exception("Argument not provided to function, please provide a workflow name.")
     workflow = get_workflow(workflow_name)
-    source_client = Algorithmia.client(api_key=source_api_key, api_address=workflow['source_info']['cluster_address'], ca_cert=source_ca_cert)
-    destination_client = Algorithmia.client(api_key=destination_api_key, api_address=destination_api_address, ca_cert=destination_ca_cert)
+    source_client = Algorithmia.client(api_key=source_api_key, api_address=workflow['source_info']['cluster_address'],
+                                       ca_cert=source_ca_cert)
+    destination_client = Algorithmia.client(api_key=destination_api_key, api_address=destination_api_address,
+                                            ca_cert=destination_ca_cert)
     print("------- Starting Algorithm Export/Import Procedure -------")
     entrypoint_algo = create_workflow(workflow, source_client, destination_aems_master, destination_client)
     print("------- Workflow Created, initiating QA Test Procedure -------")
