d73ba338d74ec590af5b6b87c73df3d80bd25e15
zeryx <james@algorithmia.com>
2021-02-02 13:48:52 -0600

    got diagnostic printing to work

--- src/algorithm_creation.py
+++ src/algorithm_creation.py
@@ -72,7 +72,7 @@ def update_algorithm(algo, remote_client, workspace_path, artifact_path):
 
     clone_bake = sh.git.bake(C=workspace_path)
     publish_bake = sh.git.bake(C=repo_path)
-    clone_bake.clone(git_path)
+    print(str(clone_bake.clone(git_path)))
     sh.rm("-r", f"{repo_path}/src")
     sh.cp("-R", f"{artifact_path}/src", f"{repo_path}/src")
     sh.cp("-R", f"{artifact_path}/requirements.txt", f"{repo_path}/requirements.txt")
@@ -80,9 +80,8 @@ def update_algorithm(algo, remote_client, workspace_path, artifact_path):
     try:
         publish_bake.add(".")
         publish_bake.commit(m="automatic initialization commit")
-        for line in publish_bake.status(_iter=True, _err_to_out=True, _out_bufsize=100):
-            print(line)
-        publish_bake.push()
+        print(str(publish_bake.status()))
+        print(str(publish_bake.push()))
     except Exception as e:
         if "Your branch is up to date with" not in str(e):
             raise e
