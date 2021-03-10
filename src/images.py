0dc791e2fa0cf89424bbe87706953a644d8c8366
CI <ci@algorithmia.com>
2021-03-10 11:28:50 -0400

    updated image ids on test (ids have been changed since last release)

--- src/images.py
+++ src/images.py
@@ -1,6 +1,6 @@
 
 PROD_IMAGES={
-    "python3": "7498490e-90fe-4b8e-b65d-3ebf3ea6c5cb",
+    "python3": "fbe2e978-9deb-494d-a2d1-e07e121d5ba0",
     "python2": "e9088b81-b781-4df3-8cda-c064f607244b",
     "pytorch-1.7": "f825234b-e2c4-4a61-83a6-74378514dcf0",
     "tensorflow-1.14": "7885d20f-5165-4bcd-b5ac-0c5fb847b883",
@@ -8,9 +8,9 @@ PROD_IMAGES={
 }
 TEST_IMAGES={
     "python3": "4beb6189-3e18-4a7a-a466-473bebe68a9f",
-    "python2": "f7ed7c9b-d205-48ef-9ddf-61880b96c1f4",
-    "pytorch-1.7": "5b027084-5fa7-431a-bdbd-f4f8eefc6ae1",
-    "tensorflow-1.14": "27d3d02d-1da0-4868-986e-e0c6937e7d16",
-    "tensorflow-2.3": "e9c1291c-1295-41b9-a4c5-30f7f44920aa",
+    "python2": "a6d09f0a-802e-436e-9343-00f8fcbb92c9",
+    "pytorch-1.7": "89881307-b29d-4e11-8042-d3b72a89b0b9",
+    "tensorflow-1.14": "b2fc4afc-9870-4c7a-ba05-342395c79caa",
+    "tensorflow-2.3": "63353687-6259-4904-a222-c567c9eb3c31",
     "python2-gpu": "81a527be-1721-4ef4-b1e2-b4d3997dd758"
 }
